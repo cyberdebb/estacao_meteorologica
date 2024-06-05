ae3a47649995b7ec70f3ceea5959473350838adf
Vini <105316221+vini-muchulski@users.noreply.github.com>
2024-06-05 18:14:14 -0300

    Add files via upload

--- esp_to_wifi/dht_temp_WIFI/teste_api.py
+++ esp_to_wifi/dht_temp_WIFI/teste_api.py
@@ -4,18 +4,18 @@ import json
 
 def enviar_dados(dados, url):
     headers = {'Content-Type': 'application/json'}
+    print(dados)
     response = requests.post(url, data=json.dumps(dados), headers=headers)
     print(response.status_code, response.text)
     #return response.status_code, response.text
 
  
-post_data = '{"temperatura": 18.6, "umidade": 87, "data": "19:48:46 01/06/2024"}'
+post_data = '{"data": "19:48:46 01/06/2024", "temperatura": 18.6, "umidade": 87 }'
 
 dados = json.loads(post_data)  # Tenta analisar a string como JSON
-print("Dados recebidos:", dados)
 
-url = "https://estacao-meteorologica-im7c10its-ludmilas-projects-fb4d1943.vercel.app/dht"
-
+#url = "https://estacao-meteorologica-im7c10its-ludmilas-projects-fb4d1943.vercel.app/dht"
+url = "https://estacao-meteorologica.vercel.app/dht"
 
 enviar_dados(dados, url)      
             
